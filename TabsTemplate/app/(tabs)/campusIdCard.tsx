36542d0469732e36f8086b102b5472df65b3b6e4
wlasitz-jeremy <wlasitz@gmail.com>
2026-09-08 17:43:34 -0600

    added campusIdCard .tsx file to project

---
+++ TabsTemplate/app/(tabs)/campusIdCard.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export default function IdCardScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Campus Id Card</Text>
+    </SafeAreaView>
+  );
+}
