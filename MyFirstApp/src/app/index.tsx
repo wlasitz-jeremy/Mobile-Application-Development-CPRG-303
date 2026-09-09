953bd40223ece6ce927e36043985ac11b2f9de99
wlasitz-jeremy <wlasitz@gmail.com>
2026-09-08 18:35:29 -0600

    created campus Id Card

--- MyFirstApp/src/app/index.tsx
+++ MyFirstApp/src/app/index.tsx
@@ -3,13 +3,30 @@ import { StyleSheet, View, Text, Image, SafeAreaView } from "react-native";
 export default function IdCardScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.card}></View>
-      <Text>Campus Id Card</Text>
+      <View style={styles.card}>
+        <Text style={styles.title}>SAIT CAMPUS ID</Text>
+        <Image source={{uri: "https://picsum.photos/200"}} style={styles.photo} />
+        <View style={styles.infoRow}>
+          <Text style={styles.name}>Jeremy Wlasitz</Text>
+          <Text style={styles.studentId}>ID: 000123456</Text>
+        </View>
+        <View style={styles.badge}>
+          <Text style={styles.badgeText}>Software Development</Text>
+        </View>
+        </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+    color: "#0a66c2",
+  },
+
   container: {
     flex: 1,
     justifyContent: "center",
@@ -25,4 +42,42 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     elevation: 4,
   },
+
+  photo: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    alignSelf: "center",
+    marginBottom: 20,
+  },
+
+  infoRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+
+  name: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+  studentId: {
+    fontSize: 16,
+    color: "#555",
+  },
+
+  badge: {
+    backgroundColor: "#0a66c2",
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    alignSelf: "flex-start",
+  },
+
+  badgeText: {
+    color: "white",
+    fontWeight: "600",
+  }
 });
