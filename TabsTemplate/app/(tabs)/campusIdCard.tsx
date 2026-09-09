7281828ff082156c9c3bb52a4a343f30f6ff8cce
wlasitz-jeremy <wlasitz@gmail.com>
2026-09-08 18:01:41 -0600

    removed campusidcard file

--- TabsTemplate/app/(tabs)/campusIdCard.tsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, Text } from 'react-native';
-
-export default function IdCardScreen() {
-  return (
-    <SafeAreaView>
-      <Text>Campus Id Card</Text>
-    </SafeAreaView>
-  );
-}
