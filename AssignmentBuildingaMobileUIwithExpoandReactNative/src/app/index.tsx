fed0f8a727f1542e88a845b6243ae6e1461661fc
wlasitz-jeremy <wlasitz@gmail.com>
2026-09-12 13:42:48 -0600

    correctly added typescript template for first assignment and reset project

--- AssignmentBuildingaMobileUIwithExpoandReactNative/src/app/index.tsx
+++ AssignmentBuildingaMobileUIwithExpoandReactNative/src/app/index.tsx
@@ -1,12 +1,15 @@
+import React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-export default function Index() {
+export default function IdCardScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.app}>
+        <Text style={styles.title}>Edit src/app/index.tsx to edit this screen.</Text>
+        </View>
     </SafeAreaView>
-  );
+);
 }
 
 const styles = StyleSheet.create({
@@ -15,4 +18,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  app: {},
+  title: {},
 });
