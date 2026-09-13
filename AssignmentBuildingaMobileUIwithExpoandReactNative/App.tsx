399b18df1493ee47c4785f022341cd4e549f3b03
wlasitz-jeremy <wlasitz@gmail.com>
2026-09-13 09:18:08 -0600

    fiddled wiht expo go and its components using reactnative

--- AssignmentBuildingaMobileUIwithExpoandReactNative/App.tsx
+++ AssignmentBuildingaMobileUIwithExpoandReactNative/App.tsx
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
 
 
 export default function App() {
   return (
-      <View style={styles.container}>
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <StatusBar style="auto" />
+      <View style={styles.appContainer}>
+        <View style={styles.container}>
+          <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+        </View>
       </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container: {
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '50%',
+    height: '50%',
+    borderRadius: 50,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
