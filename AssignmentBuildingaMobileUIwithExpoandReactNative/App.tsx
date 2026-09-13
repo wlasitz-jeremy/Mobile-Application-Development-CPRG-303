7666a47c6db0ce1eebf6680609965c4bdb0273a8
wlasitz-jeremy <wlasitz@gmail.com>
2026-09-13 12:15:55 -0600

    added scrollview test function in App.tsx for assignment 1

--- AssignmentBuildingaMobileUIwithExpoandReactNative/App.tsx
+++ AssignmentBuildingaMobileUIwithExpoandReactNative/App.tsx
@@ -4,9 +4,107 @@ import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
 export default function App() {
   return (
       <View style={styles.appContainer}>
+        <ScrollView style={styles.scrollView}>
         <View style={styles.container}>
           <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
+          <Text>Testing to see if the scroll view works</Text>
         </View>
+        </ScrollView>
       </View>
   );
 }
@@ -16,15 +114,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  scrollView: {
+    marginTop: 60,
   },
   container: {
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '50%',
-    height: '50%',
-    borderRadius: 50,
+    backgroundColor: '#767676',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // width: '100%',
+    // height: '93%',
   },
   text: {
     color: '#fff',
